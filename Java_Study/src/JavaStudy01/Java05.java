a9288dffc35553c1ced9130a96018699c75a2b30
h95fit <h95fit@gmail.com>
2022-11-18 19:35:09 +0900

    JAVA 문자열 연산자 기초

--- Java_Study/src/JavaStudy01/Java05.java
+++ Java_Study/src/JavaStudy01/Java05.java
@@ -21,6 +21,7 @@ package JavaStudy01;
  문자열 연산자
  문자열과 문자열을 연결하여 하나의 긴 문자열 생성
  특징 : 문자열과 기본 타입을 +로 연산 시 모두 문자열로 변환된다!
+ "" 큰따옴표 사이에 있는 모든 것은 문자열
  
 */
 
@@ -72,6 +73,36 @@ public class Java05 {
 		System.out.println(str3);
 		
 		System.out.println("100" + 200);  // 문자열과 기본타입 연산 시 문자열로 자동 형 변환됨!
+
+		
+		//문자열 : 자바 언어에서 레퍼런스 타입으로 구성되어 있는 여러 개의 문자를 출력하기 위해 존재하는 데이터 타입
+		//문자열은 워낙 많이 사용하기 때문에 레퍼런스 타입이지만 기본 타입처럼 사용함
+		//자바 언어에서 " " 사이에 존제하는 데이터는 모두 문자열임
+		//문자열 연결 연산자 + : 2개 이상의 문자열을 하나의 문자열로 연결하는 연산자
+		//문자열과 다른 데이터 타입을 +로 연산할 경우 둘다 문자열로 변환됨
+		//문자열 사용 방법 : String 변수명; (메모리 등록만 진행, 실제 데이터 없음, 사용 전 초기화 필수)
+		              //String 변수명 = "문자열 데이터"; (가장 일반적인 방법, 선언과 함께 데이터 저장(초기 데이터 저장))
+		              //String 변수명 = new String("문자열 데이터");
+		
+		String str5;
+		String str6 = "JAVA 강의 3일차";
+		String str7 = new String("문자열 연산자");
+		
+		str5 = "Home";
+		
+		System.out.println(str5);  // 초기화하지 않으면 에러 발생
+		System.out.println(str6);
+		System.out.println(str7);
+		
+		System.out.println();
+		
+		System.out.println(str5 + str6);
+		
+		System.out.println();
+		
+		System.out.println(2022 + " JAVA 수업");
+		System.out.println("JAVA 수업" + 20 + 22);
+		System.out.println("JAVA 수업" + 20 * 22);  //곱하기는 더하기보다 우선순위이므로 숫자 연산 후 문자열 연산이 이루어진다
 	}
 
 }
