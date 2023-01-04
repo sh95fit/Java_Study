1c830fa178ec4c52534a7cf2d91eff76132dcec8
h95fit <h95fit@gmail.com>
2023-01-04 19:23:14 +0900

    Java 파일 입출력 부분 변경(try-catch)

--- Java_Study/src/JavaStudy01/Java40_3.java
+++ Java_Study/src/JavaStudy01/Java40_3.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 
 // try-with-resources
+// (자동 리소스 닫기! 즉 close() 메서드를 호출해 리소스를 닫을 필요가 없음!)
 // finally의 번거로움을 해소해줌
 // 예외 발생 여부와 상관없음
 // 사용했던 리소스 객체의 close() 메소드를 자동으로 호출해 리소스를 닫는다
@@ -20,16 +21,25 @@ public class Java40_3 {
 	public static void main(String[] args) {
 		FileInputStream input = null;
 		
+//		외부 리소스 사용을 위해 예외 처리 구문 사용
 		try {
 			File file = new File("C:\\Users\\tpgns\\Desktop\\Hun's\\Hun's\\sh95fit_git\\Java_Study\\Java_Study\\src\\JavaStudy01\\Java40_3.txt");
-			FileReader filereader = new FileReader(file);
-			BufferedReader bufReader = new BufferedReader(filereader);
-			String line = "";
-			while ((line = bufReader.readLine()) != null) {
-				System.out.println(line);
-
+//			FileReader filereader = new FileReader(file);
+//			BufferedReader bufReader = new BufferedReader(filereader);
+//			String line = "";
+//			while ((line = bufReader.readLine()) != null) {
+//				System.out.println(line);
+//
+//			}
+//			bufReader.close();
+			input = new FileInputStream(file);
+			
+			int i = 0;
+			
+			while ((i = input.read()) != -1) {
+				System.out.write(i);
 			}
-			bufReader.close();
+			
 		}
 		catch (IOException e) {
 			System.out.println(e);
